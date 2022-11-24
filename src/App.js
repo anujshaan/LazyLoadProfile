d7ae49ffaa325d075ea1f625d6037b2b58440164
anujshaan <anujshaan261@gmail.com>
2022-11-24 14:35:59 +0530

    added lazyLoad

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Pages/Home";
 function App() {
   return (
     <div className="App">
+      <h1 style={{textAlign:'center'}}>LAZY Loading List of user when scrolling !!</h1>
       <Home/>
     </div>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,60 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react';
+import clsx from 'clsx';
+import { ThreeCircles } from 'react-loader-spinner'
+
+import useLazyLoad from '../useLazyLoad';
 import UserCard from '../Components/UserCard'
+
+const NUM_PER_PAGE = 3;
+const TOTAL_PAGE = 4;
 
 const Home = () => {
     const [allUser, setAllUser] = useState([])
+    const triggerRef = useRef(null)
     
     useEffect(()=>{
       fetch('https://jsonplaceholder.typicode.com/users/')
-        .then((response)=> response.json())
-        .then((json)=> {setAllUser(json)})
+        .then((res)=> res.json())
+        .then((data)=> {setAllUser(data)})
+        .catch((err) => console.log(err))
     },[])
+
+    const onGrabData = (currentPage) =>{
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const userData = allUser.slice(
+                    ((currentPage - 1)%TOTAL_PAGE) * NUM_PER_PAGE,
+                    NUM_PER_PAGE * (currentPage%TOTAL_PAGE)
+                );
+                console.log(userData);
+                resolve(userData);
+            }, 3000);
+        });
+    }
+
+    const {data, loading} = useLazyLoad({triggerRef, onGrabData})
+
+
   return (
-    <div>
-        {
-            allUser.map((user)=>(
-                <UserCard key={user.id} user={user}/>
-            ))
-        }
-    </div>
+    <>
+        <div>
+            {
+                data.map((user)=>(
+                    <UserCard key={user.id} user={user}/>
+                ))
+            }
+        </div>
+        <div ref={triggerRef} className={clsx("trigger", {visible:loading})}>
+            <div className='spinnerWrapper'>
+            <ThreeCircles
+                color='#00bfff'
+                height={80}
+                width={200}
+                className='spinner'
+            />
+            </div>
+        </div>
+    </>
   )
 }
 
